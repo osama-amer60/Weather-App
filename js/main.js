e2ab21c4546abbb8839844f8cd9c5dccba9b6375
osama-amer60 <osamaamerr97@gmail.com>
2022-09-25 11:06:44 +0300

    finish..

--- js/main.js
+++ js/main.js
@@ -10,8 +10,9 @@ let  windSpeed = document.getElementById("windSpeed")
 let humidity = document.getElementById("humidity")
 let goBack = document.getElementById("goBack")
 let popupmessage = document.getElementById("popupmessage")
-
-
+let getDeviceLocation = document.getElementById("getDeviceLocation")
+let getApi;
+let response
 goBack.addEventListener("click",function(){
     location.reload()
 })
@@ -22,27 +23,25 @@ getWeatherBtn.addEventListener("click",function(){
 
 })
 
+getDeviceLocation.addEventListener("click",function(){
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(onSuccess,onError)
 
+    }else{
+        alert("your browser not support geolocation api")
+    }
+})
 
 async function getWeather(city){
-    let getApi = await fetch(`https://api.weatherapi.com/v1/current.json?key=d4f8b5d0cae44ecaa16122704222309&q=${city}&aqi=no`)
-    let response = await getApi.json()
+     getApi = await fetch(`https://api.weatherapi.com/v1/current.json?key=d4f8b5d0cae44ecaa16122704222309&q=${city}&aqi=no`)
+     response = await getApi.json()
 
 
     if (response.error) {
         popupmessage.classList.remove("d-none")
     }else{
-        popupmessage.classList.remove("warning")
-        popupmessage.classList.add("getLocation")
-        popupmessage.classList.remove("d-none")
-
-
-        disImg.src = response.current.condition.icon
-        temp.innerHTML = response.current.feelslike_c
-        disCity.innerHTML = response.location.name
-        disCountry.innerHTML = response.location.country
-        windSpeed.innerHTML = response.current.wind_kph
-        humidity.innerHTML = response.current.humidity
+        popupMess()
+        show()
         setTimeout(display,1000)
     }
 }
@@ -50,4 +49,32 @@ async function getWeather(city){
 function display(){
     setting.classList.add("d-none")
     displayTheWeather.classList.remove("d-none")
+}
+
+async function onSuccess(position){
+    const {latitude, longitude} = position.coords;
+     getApi = await fetch(`https://api.weatherapi.com/v1/current.json?key=d4f8b5d0cae44ecaa16122704222309&q=${latitude,longitude}&aqi=no`)
+     response = await getApi.json()
+    console.log(response)
+    popupMess()
+    show()
+    setTimeout(display,1000)
+
+}
+function popupMess(){
+    popupmessage.classList.remove("warning")
+    popupmessage.classList.add("getLocation")
+    popupmessage.classList.remove("d-none")
+}
+function show(){
+    disImg.src = response.current.condition.icon
+    temp.innerHTML = response.current.feelslike_c
+    disCity.innerHTML = response.location.name
+    disCountry.innerHTML = response.location.country
+    windSpeed.innerHTML = response.current.wind_kph
+    humidity.innerHTML = response.current.humidity
+}
+function onError(){
+    popupmessage.classList.remove("d-none")
+
 }
